4827673631758a40725356d74d940587b7bf64e6
monikaez <86738350+monikaez@users.noreply.github.com>
2024-01-20 00:04:01 +0200

    03

---
+++ Workshops and Exercises/03.TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitterApp.Models;
+using TextSplitterApp.ViewModels;
+
+namespace TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+        public IActionResult Index(TextSplitViewModel textSplitViewModel)
+        {
+            return View(textSplitViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextSplitViewModel textSplitViewModel)
+        {
+            if (String.IsNullOrWhiteSpace(textSplitViewModel.TextToSplit))
+            {
+                return this.RedirectToAction("Index", new TextSplitViewModel()
+                {
+                    TextToSplit = string.Empty,
+                    SplitText = string.Empty
+                });
+            }
+            string[] words = textSplitViewModel.TextToSplit
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string splitText = String.Join(Environment.NewLine, words);
+
+            textSplitViewModel.SplitText = splitText;
+            return this.RedirectToAction("Index", textSplitViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Workshops and Exercises/03.TextSplitterApp/ViewModels/TextSplitViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TextSplitterApp.ViewModels
+{
+    public class TextSplitViewModel
+    {
+        public string? TextToSplit { get; set; }
+
+        public string? SplitText { get; set; }
+    }
+}
